96f071dc0ba6eea375c413cb5ec36ad5089d98e0
AdrienMnc <adrien.moncieu@gmail.com>
2022-06-16 15:35:21 +0200

    last ad

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once __DIR__ . "/auto_enchere/pages/connexion_page.php";
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Document</title>
+</head>
+
+<body>
+
+  <p>bonjour</p>
+
+
+  <?php afficher_connexion_page(); ?>
+
+</body>
+
+</html>
