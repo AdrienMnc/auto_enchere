12d698b78491fba1342e225ab6475f8c6fe7d4fc
AdrienMnc <adrien.moncieu@gmail.com>
2022-06-17 15:11:14 +0200

    lien dead

---
+++ includes/deco.php
@@ -0,0 +1,5 @@
+<?php
+session_start();
+//session_destroy sert à detruire la session  
+session_destroy();
+echo " Vous êtes déconnecté";
--- includes/profil.incl.php
+++ includes/profil.incl.php
@@ -1,2 +1,6 @@
 <p>Votre adresse email est <?= $_SESSION["utilisateur_email"] ?> et votre id <?= $_SESSION["utilisateur_id"] ?> .</p>
 <a href="/pages/profil_utilisateur.page.php">Connecter à <?= $_SESSION["utilisateur_prenom"] ?> <?= $_SESSION["utilisateur_nom"] ?></a>
+
+<form id='fermer' name='fermer' method='post' action="/deco.php">
+    <input type='submit' id='soumettre' value='Se déconnecter' style='width:120px;height:25px;' />
+</form>
